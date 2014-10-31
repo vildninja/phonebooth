fb5dcf9d5b47678c26c7c03225a18fbda37db4f3
Julian <julian@contingent.dk>
2014-10-31 12:04:11 +0100

    phone with button

---
+++ Assets/scripts/ButtonScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour {
+
+	public int value = -1;
+
+	private TextMesh label;
+	public KeyCode key;
+
+	public PhoneController phone;
+
+	// Use this for initialization
+	void Start () {
+
+
+		// debug stuff
+		TextMesh[] t = this.GetComponentsInChildren<TextMesh> ();
+		TextMesh label = t[0];
+		this.label = label;
+		this.label.text = value.ToString();
+
+	}
+
+	public void activate() {
+		phone.OnButtonPress (value);	
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp(key)) {
+			activate();
+		}
+	}
+
+}
---
+++ Assets/scripts/PhoneController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhoneController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+	public void OnButtonPress(int value) {
+
+		Debug.Log (value);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
