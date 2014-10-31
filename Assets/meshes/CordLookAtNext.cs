e4e150f62cdaa485a5ce1a72a7e81dbc7678ae04
Jannek Egeskov Kristensen <jannek.ek@gmail.com>
2014-10-31 17:16:35 +0100

    Phonebooth

---
+++ Assets/meshes/CordLookAtNext.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CordLookAtNext : MonoBehaviour {
+
+    Transform target;
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        if (target)
+        {
+            transform.LookAt(target);
+        }
+        else
+        {
+            target = GetComponent<SpringJoint>().connectedBody.transform;
+        }
+	}
+}
---
+++ Assets/meshes/PhoneCordCreator.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhoneCordCreator : MonoBehaviour {
+
+    public Transform phonebooth;
+    public Transform handle;
+    public Transform cordParent;
+
+	// Use this for initialization
+	void Start ()
+    {
+        cordParent.parent = phonebooth;
+        var p = cordParent;
+        while (p.childCount > 0)
+        {
+            var c = p.GetChild(0);
+            c.parent = phonebooth;
+            var joint = p.GetComponent<SpringJoint>();
+            joint.connectedBody = c.rigidbody;
+            joint.minDistance = Vector3.Distance(c.position, p.position) / 4;
+            joint.maxDistance = Vector3.Distance(c.position, p.position) / 4;
+            joint.spring = 9999999;
+            p = c;
+        }
+        p.GetComponent<SpringJoint>().connectedBody = handle.rigidbody;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+	
+	}
+}
