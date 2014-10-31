50a2570ce46a3f52e83efe48bff24fcb9b8a7229
Julian <julian@contingent.dk>
2014-10-31 12:25:09 +0100

    add sound

--- Assets/scripts/ButtonScript.cs
+++ Assets/scripts/ButtonScript.cs
@@ -5,6 +5,8 @@ public class ButtonScript : MonoBehaviour {
 
 	public int value = -1;
 
+	public AudioSource buttonSound;
+
 	private TextMesh label;
 	public KeyCode key;
 
@@ -19,18 +21,24 @@ public class ButtonScript : MonoBehaviour {
 		TextMesh label = t[0];
 		this.label = label;
 		this.label.text = value.ToString();
-
 	}
 
+
 	public void activate() {
+		PlayButtonSound ();
+
 		phone.OnButtonPress (value);	
 	}
 
+	void PlayButtonSound ()
+	{
+		buttonSound.Play ();
+	}
 
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyUp(key)) {
+		if (Input.GetKeyDown(key)) {
 			activate();
 		}
 	}
