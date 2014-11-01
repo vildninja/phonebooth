2514ba45cedcc237bf91ac91010907c8a9d9b1b6
Jannek Egeskov Kristensen <jannek.ek@gmail.com>
2014-11-01 14:18:25 +0100

    book and audio batch

---
+++ Assets/Editor/AudioBatchSettings.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class AudioBatchSettings {
+
+    [MenuItem("Custom/Audio to 2D")]
+    public static void AudioTo2D()
+    {
+        foreach (AudioClip audio in Selection.GetFiltered(typeof(AudioClip), SelectionMode.Assets))
+        {
+            var path = AssetDatabase.GetAssetPath(audio);
+            var importer = AudioImporter.GetAtPath(path) as AudioImporter;
+            importer.threeD = false;
+            AssetDatabase.ImportAsset(path);
+        }
+    }
+}
