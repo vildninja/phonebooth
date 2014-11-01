d76335b2a7aa47effb295836d1d1a8b075eb2db9
Jannek Egeskov Kristensen <jannek.ek@gmail.com>
2014-11-01 15:50:14 +0100

    Added intro scene - missing audio

---
+++ Assets/scripts/LoadOnClick.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadOnClick : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit) && hit.collider == collider)
+            {
+                Application.LoadLevel(Application.loadedLevel + 1);
+            }
+        }
+	}
+}
