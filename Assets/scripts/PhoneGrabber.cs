ee44205d7e4de570e96a4cecbffd62a10f6e619c
Jannek Egeskov Kristensen <jannek.ek@gmail.com>
2014-11-01 14:58:42 +0100

    Grab that phone

---
+++ Assets/scripts/PhoneGrabber.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhoneGrabber : MonoBehaviour {
+
+    public Transform handle;
+
+	// Use this for initialization
+	IEnumerator Start () {
+        yield return new WaitForSeconds(1f);
+
+        while (Vector3.Distance(handle.position, transform.position) > 0.1f)
+        {
+            handle.position = Vector3.Lerp(handle.position, transform.position, 0.05f);
+            handle.rotation = Quaternion.Lerp(handle.rotation, transform.rotation, 0.05f);
+            yield return null;
+        }
+
+        handle.parent = transform;
+	}
+}
