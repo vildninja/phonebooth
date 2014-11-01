e8e3dd14678e0356fd38aab3628da93dfd3af634
Julian <julian@contingent.dk>
2014-11-01 15:02:22 +0100

    k

--- Assets/scripts/PhoneLook.cs
+++ Assets/scripts/PhoneLook.cs
@@ -17,7 +17,7 @@ public class PhoneLook : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        Debug.Log(Input.mousePosition.x + " / " + Screen.width + " = " + (Input.mousePosition.x / Screen.width) + " -> " + (curve.Evaluate(Input.mousePosition.x / Screen.width) * horizontal));
+        //Debug.Log(Input.mousePosition.x + " / " + Screen.width + " = " + (Input.mousePosition.x / Screen.width) + " -> " + (curve.Evaluate(Input.mousePosition.x / Screen.width) * horizontal));
 
         transform.rotation = Quaternion.Euler(curve.Evaluate(Input.mousePosition.y / Screen.height) * vertical, curve.Evaluate(Input.mousePosition.x / Screen.width) * horizontal, 0);
 	}
